f94a7c2748da3e9e7538cdc3dc66ca2ce2b4242f
shinyEazy <tanhngusi39@gmail.com>
2025-03-11 16:51:57 +0700

    hmm

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
+## Project Setup
+
+### Frontend Setup
+
+```sh
 cd frontend
 npm install
 npm start
+```
 
+### Backend Setup
+
+```sh
 cd backend
+pip install -r requirements.txt
+python create_db.py
 uvicorn app.main:app --reload --port 3001
+```
--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.config import settings  # Now this will work
+from app.config import settings 
 
 SQLALCHEMY_DATABASE_URL = (
     f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}"
