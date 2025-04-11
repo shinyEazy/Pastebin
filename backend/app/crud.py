a49c355d81a1413f3433e0d341ea4f585c4d4f56
tuananhlehuy1111 <tuananhlehuy1111@gmail.com>
2025-04-11 10:57:56 +0700

    testing uuid

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -14,7 +14,7 @@ def create_paste(db: Session, paste: schemas.PasteCreate, redis):
     redis.expire(paste_key, 10)
     return paste_schema
 
-def get_paste(db, paste_id: int, redis):
+def get_paste(db, paste_id: str, redis):
     paste_key = f"paste:{paste_id}"
     increment_key = f"paste:{paste_id}:views_increment"
     cached_paste = redis.get(paste_key)
--- backend/app/models/paste.py
+++ backend/app/models/paste.py
@@ -2,13 +2,14 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, Text, Boolean
 from app.database import Base  
 import pytz
+import uuid
 
 local_tz = pytz.timezone("Asia/Bangkok")  
         
 class Paste(Base):
     __tablename__ = "pastes"
     
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(String(36), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     content = Column(Text, nullable=False)
     language = Column(String(50), default="plaintext")
     created_at = Column(DateTime, default=lambda: datetime.now(local_tz))
